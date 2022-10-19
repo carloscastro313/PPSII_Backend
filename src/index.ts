ddb031c108a325325e6ddd17174f9b4ae28da909
castrocarlos313 <castrocarlos313@gmail.com>
2022-10-19 00:13:07 -0300

    fix package.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { App } from './app';
+import { App } from "./app";
 import "dotenv/config";
 
 async function main() {
-  const app = new App(3000);
+  const app = new App(process.env.PORT || 3000);
   await app.listen();
 }
 
