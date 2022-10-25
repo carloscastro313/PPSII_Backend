758c28539e47beb851e54b59ebee7db98eab470b
castrocarlos313 <castrocarlos313@gmail.com>
2022-10-25 18:06:48 -0300

    Fix pool db

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,18 @@
-import { MySQL2Extended } from 'mysql2-extended';
-import { createPool } from 'mysql2/promise';
+import { MySQL2Extended } from "mysql2-extended";
+import { createPool, Pool } from "mysql2/promise";
 
-async function getInstanceDB(){
+var globalPool: Pool | undefined = undefined;
 
-    const connection = await createPool({
-        host: process.env.HOST,
-        user: process.env.USUARIO,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD,
-        connectionLimit: 50
-    });
-    return new MySQL2Extended(connection);
+async function getInstanceDB(): Promise<MySQL2Extended> {
+  if (globalPool) return new MySQL2Extended(globalPool);
+
+  globalPool = await createPool({
+    host: process.env.HOST,
+    user: process.env.USUARIO,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+  });
+  return new MySQL2Extended(globalPool);
 }
 
 export default getInstanceDB;
