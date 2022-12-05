74df50cf48ad9a229aacc1f8e4dfefc73096d03f
castrocarlos313 <castrocarlos313@gmail.com>
2022-12-05 19:54:01 -0300

    Mejoras mail

--- src/controllers/avisos.controller.ts
+++ src/controllers/avisos.controller.ts
@@ -1,134 +1,175 @@
-import { Request, Response } from 'express'
-import { errorMsg } from '../const/errors';
-import getInstanceDB  from '../database'
-import { EstadosAlumnoMateria } from '../enums/estadoAlumnoMateria';
-import { TiposUsuario } from '../enums/tiposUsuario';
-import { getTokenId } from '../helpers/jwt';
-import mandarMail from '../helpers/mailer';
-import AlumnoMaterias from '../interface/AlumnoMaterias';
-import Avisos from '../interface/Avisos';
-import AvisoUsuarios from '../interface/AvisoUsuarios';
-import MateriaDivision from '../interface/MateriaDivision';
-import Usuario from '../interface/Usuario';
+import { Request, Response } from "express";
+import { errorMsg } from "../const/errors";
+import getInstanceDB from "../database";
+import { EstadosAlumnoMateria } from "../enums/estadoAlumnoMateria";
+import { TiposUsuario } from "../enums/tiposUsuario";
+import { getTokenId } from "../helpers/jwt";
+import mandarMail from "../helpers/mailer";
+import AlumnoMaterias from "../interface/AlumnoMaterias";
+import Avisos from "../interface/Avisos";
+import AvisoUsuarios from "../interface/AvisoUsuarios";
+import MateriaDivision from "../interface/MateriaDivision";
+import Usuario from "../interface/Usuario";
 
-export async function getAvisos(req: Request, res: Response): Promise<Response>{
-    try{
-        const db = await getInstanceDB();
-        const avisos = await db.select<Avisos>("Avisos");
+export async function getAvisos(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const db = await getInstanceDB();
+    const avisos = await db.select<Avisos>("Avisos");
 
-        return res.json(avisos);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    return res.json(avisos);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
 
-export async function getAvisoById(req: Request, res: Response): Promise<Response>{
-    const idAviso = req.params.idAviso;
+export async function getAvisoById(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const idAviso = req.params.idAviso;
 
-    try{
-        const db = await getInstanceDB();
-        const aviso = await db.select<Avisos>("Avisos",{Id: idAviso});
+  try {
+    const db = await getInstanceDB();
+    const aviso = await db.select<Avisos>("Avisos", { Id: idAviso });
 
-        return res.json(aviso);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    return res.json(aviso);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
 
-export async function createAviso(req: Request, res: Response): Promise<Response>{
-    const newAviso = req.body;
-    var idAviso = 0;
-    const bearerToken = req.header("authorization") as string;
-    const { id } = getTokenId(bearerToken);
+export async function createAviso(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const newAviso = req.body;
+  var idAviso = 0;
+  const bearerToken = req.header("authorization") as string;
+  const { id } = getTokenId(bearerToken);
 
-    try{
-        const db = await getInstanceDB();
+  let mails: string[] = [];
 
-        await db.transaction(async (t) => {
+  try {
+    const db = await getInstanceDB();
 
-            var aviso : Avisos = {IdEmisor: id,Titulo: newAviso.Titulo, Mensaje: newAviso.Mensaje}
+    await db.transaction(async (t) => {
+      var aviso: Avisos = {
+        IdEmisor: id,
+        Titulo: newAviso.Titulo,
+        Mensaje: newAviso.Mensaje,
+      };
 
-            await t.insert<Avisos>("Avisos",aviso);
-            idAviso = await t.getLastInsertId();
+      await t.insert<Avisos>("Avisos", aviso);
+      idAviso = await t.getLastInsertId();
 
-            for(let i = 0; i < newAviso.Receptores.length; i++){
-                var newAvisoUsuario : AvisoUsuarios = { IdAviso: idAviso, IdUsuario: newAviso.Receptores[i], Leido: 0 }
+      for (let i = 0; i < newAviso.Receptores.length; i++) {
+        var newAvisoUsuario: AvisoUsuarios = {
+          IdAviso: idAviso,
+          IdUsuario: newAviso.Receptores[i],
+          Leido: 0,
+        };
 
-                await t.insert<AvisoUsuarios>("AvisoUsuarios",{...newAvisoUsuario});
+        await t.insert<AvisoUsuarios>("AvisoUsuarios", { ...newAvisoUsuario });
 
-                //MANDAR MAIL
-                var usuario = await db.query<Usuario>("SELECT * FROM Usuarios WHERE Id = ?",[newAviso.Receptores[i]]);
-                var usuariosMails = usuario.map(
-                    ({ Mail }) => Mail as string
-                );
+        //MANDAR MAIL
+        var usuario = await db.query<Usuario>(
+          "SELECT * FROM Usuarios WHERE Id = ?",
+          [newAviso.Receptores[i]]
+        );
+        var usuariosMails = usuario.map(({ Mail }) => Mail as string);
+        mails = [...mails, ...usuariosMails];
+      }
+    });
 
-                await mandarMail(usuariosMails,"NUEVO AVISO: "+newAviso.Titulo,newAviso.Mensaje,"");
-            }
-        })
-        
-        return res.json(newAviso);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    await mandarMail(
+      mails,
+      "NUEVO AVISO: " + newAviso.Titulo,
+      newAviso.Mensaje,
+      ""
+    );
+    return res.json(newAviso);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
 
-export async function marcarLeido(req: Request, res: Response): Promise<Response>{
-    const aviso = req.body;
-    try{
-        const db = await getInstanceDB();
+export async function marcarLeido(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const aviso = req.body;
+  try {
+    const db = await getInstanceDB();
 
-        await db.update<AvisoUsuarios>("AvisoUsuarios",{ Leido: 1 },{ IdAviso: aviso.IdAviso, IdUsuario: aviso.IdUsuario});
+    await db.update<AvisoUsuarios>(
+      "AvisoUsuarios",
+      { Leido: 1 },
+      { IdAviso: aviso.IdAviso, IdUsuario: aviso.IdUsuario }
+    );
 
-        return res.json({msg: "Leido"});
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    return res.json({ msg: "Leido" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
 
-export async function traerAvisosNoLeidosPorUsuario(req: Request, res: Response): Promise<Response>{
-    const idUsuario = req.params.idUsuario;
+export async function traerAvisosNoLeidosPorUsuario(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const idUsuario = req.params.idUsuario;
 
-    try{
-        const db = await getInstanceDB();
+  try {
+    const db = await getInstanceDB();
 
-        var avisos = await db.select<AvisoUsuarios>("AvisoUsuarios",{IdUsuario: idUsuario, Leido: 0});
+    var avisos = await db.select<AvisoUsuarios>("AvisoUsuarios", {
+      IdUsuario: idUsuario,
+      Leido: 0,
+    });
 
-        return res.json(avisos);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    return res.json(avisos);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
 
-export async function traerTodosLosAvisosPorUsuario(req: Request, res: Response): Promise<Response>{
-    const idUsuario = req.params.idUsuario;
+export async function traerTodosLosAvisosPorUsuario(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const idUsuario = req.params.idUsuario;
 
-    try{
-        const db = await getInstanceDB();
+  try {
+    const db = await getInstanceDB();
 
-        var avisos = await db.select<AvisoUsuarios>("AvisoUsuarios",{IdUsuario: idUsuario});
+    var avisos = await db.select<AvisoUsuarios>("AvisoUsuarios", {
+      IdUsuario: idUsuario,
+    });
 
-        return res.json(avisos);
-    } catch (error) {
+    return res.json(avisos);
+  } catch (error) {
+    console.log(error);
 
-        console.log(error);
-        
-        return res.status(500).json({
-        msg: errorMsg.ERROR_INESPERADO,
-        });
-    }
+    return res.status(500).json({
+      msg: errorMsg.ERROR_INESPERADO,
+    });
+  }
 }
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,47 +1,54 @@
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
-export default async function mandarMail(to:string[],subject:string,text:string,html:string) {
-    try{
-        // Generate test SMTP service account from ethereal.email
+export default async function mandarMail(
+  to: string[],
+  subject: string,
+  text: string,
+  html: string
+) {
+  try {
+    // Generate test SMTP service account from ethereal.email
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: "smtp-mail.outlook.com",
-            secureConnection: false, // TLS requires secureConnection to be false
-            requireTLS:true,//this parameter solved problem for me
-            port: 587,
-            tls: {
-                ciphers:'SSLv3'
-            },
-            secure: false, // true for 465, false for other ports
-            method:"GET",
-            auth: {
-            user: "sistema-universidad-avisos@hotmail.com", // sender user
-            pass: "gtBQX.nC/e4B2D2", // sender password
-            connectionTimeout: 5 * 60 * 1000, // 5 min
-            },
-        });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com",
+      secureConnection: false, // TLS requires secureConnection to be false
+      requireTLS: true, //this parameter solved problem for me
+      port: 587,
+      tls: {
+        ciphers: "SSLv3",
+      },
+      secure: false, // true for 465, false for other ports
+      method: "GET",
+      auth: {
+        user: "sistema-universidad-avisos@hotmail.com", // sender user
+        pass: "gtBQX.nC/e4B2D2", // sender password
+        connectionTimeout: 5 * 60 * 1000, // 5 min
+      },
+    });
 
-        for (let i = 0; i < to.length; i++) {
-            // send mail with defined transport object
-            let info = await transporter.sendMail({
-                from: "sistema-universidad-avisos@hotmail.com", // sender address
-                to: to[i], // list of receivers
-                subject: subject, // Subject line
-                text: text, // plain text body
-                html: html, // html body
-            });
+    let mails = "";
 
-            console.log("Message sent: %s", info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-            // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        }
-
-    }catch(error){
-        console.log(error);
+    for (let i = 0; i < to.length; i++) {
+      mails += to[i];
+      if (to.length - 1 !== i) {
+        mails += ",";
+      }
     }
+    let info = await transporter.sendMail({
+      from: "sistema-universidad-avisos@hotmail.com", // sender address
+      to: mails, // list of receivers
+      subject: subject, // Subject line
+      text: text, // plain text body
+      html: html, // html body
+    });
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.log(error);
+  }
 }
